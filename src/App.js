148685f69f4e2d66ca6da9fbe44a4a377f0cf154
izak <izabkru88@gmail.com>
2020-08-05 13:11:21 +0100

    Created single UserItem component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/Useritem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar />
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <UserItem />
       </div>
     );
   }
